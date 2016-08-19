2add7c203d2575511cbaaf13da3ec3e0106af60a
Will Rogers <wahrogers@gmail.com>
2016-08-19 17:33:04 +0100

    Skeleton for at elements.

---
+++ pyat/elements.py
@@ -0,0 +1,33 @@
+import numpy
+
+
+class Element(object):
+    def __init__(self, length):
+        self.length = length
+
+
+class Drift(Element):
+    def __init__(self, length):
+        super(Drift, self).__init__(length)
+        self.pass_method = 'DriftPass'
+
+
+class Magnet(Element):
+    def __init__(self, length, k):
+        super(Magnet, self).__init__(length)
+        self.polynom_a = numpy.array([0,0,0,0])
+        self.polynom_b = numpy.array([0,0,0,0])
+
+
+class Quad(Element):
+    def __init__(self, length, k1):
+        super(Quad, self).__init__(length)
+        self.polynom_b[1] = k1
+        self.pass_method = 'QuadLinearPass'
+
+
+class Sext(Element):
+    def __init__(self, length, k2):
+        super(Quad, self).__init__(length)
+        self.polynom_b[2] = k2
+        self.pass_method = 'StrMPoleSymplectic4Pass'
