aa641069e5c34dc41437d2499adae9d460f4554e
Will Rogers <wahrogers@gmail.com>
2016-09-13 17:16:06 +0100

    Use four spaces for indentation in new C code.

--- atintegrators/pyutils.c
+++ atintegrators/pyutils.c
@@ -10,39 +10,39 @@ int
 void
 #endif
 init_numpy() {
-	import_array();
+    import_array();
 }
 
 long py_get_long(PyObject *element, char *name) {
-	return PyInt_AsLong(PyObject_GetAttrString(element, name));
+    return PyInt_AsLong(PyObject_GetAttrString(element, name));
 }
 
 double py_get_double(PyObject *element, char *name) {
-	return PyFloat_AsDouble(PyObject_GetAttrString(element, name));
+    return PyFloat_AsDouble(PyObject_GetAttrString(element, name));
 }
 
 double *numpy_get_double_array(PyObject *element, char *name) {
-	if (!array_imported) {
-		init_numpy();
-		array_imported = 1;
-	}
-	PyObject *array;
-	if (PyObject_HasAttrString(element, name)) {
-		array = PyObject_GetAttrString(element, name);
-	} else {
-		return NULL;
-	}
-	double *arin;
-	if (!PyArray_Check(array)) {
-		printf("%s not an array!\n", name);
-		return NULL;
-	}
-	npy_intp dims[3];
-	PyArray_Descr *descr;
-	descr = PyArray_DescrFromType(NPY_DOUBLE);
-	if (!PyArray_AsCArray((PyObject **)&array, (void *)&arin, dims, 1, descr) < 0) {
-		printf("Conversion failed.\n");
-		return NULL;
-	}
-	return arin;
+    if (!array_imported) {
+        init_numpy();
+        array_imported = 1;
+    }
+    PyObject *array;
+    if (PyObject_HasAttrString(element, name)) {
+        array = PyObject_GetAttrString(element, name);
+    } else {
+        return NULL;
+    }
+    double *arin;
+    if (!PyArray_Check(array)) {
+        printf("%s not an array!\n", name);
+        return NULL;
+    }
+    npy_intp dims[3];
+    PyArray_Descr *descr;
+    descr = PyArray_DescrFromType(NPY_DOUBLE);
+    if (!PyArray_AsCArray((PyObject **)&array, (void *)&arin, dims, 1, descr) < 0) {
+        printf("Conversion failed.\n");
+        return NULL;
+    }
+    return arin;
 }
--- pyat/at.c
+++ pyat/at.c
@@ -42,50 +42,50 @@ struct LibraryListElement* SearchLibraryList(struct LibraryListElement *head, co
 typedef void (*pass_function)(double *rin, int num_particles, PyObject *element, struct parameters *param);
 
 int call_pass_method(double *rin, int num_particles, PyObject *element, char *fn_name, struct parameters *param) {
-	char *lib_file = malloc(sizeof(char) * 1000);
-	struct LibraryListElement *LibraryListPtr;
-	void *fn_handle;
-	strcpy(lib_file, INTEGRATOR_PATH);
-	strcat(lib_file, fn_name);
-	strcat(lib_file, ".so");
-	LibraryListPtr = SearchLibraryList(LibraryList, fn_name);
-	if (LibraryListPtr) {
-		fn_handle = LibraryListPtr->FunctionHandle;
-	} else {
-		printf("Trying to load %s\n", lib_file);
-		void *dl_handle = dlopen(lib_file, RTLD_LAZY);
-		char *error = dlerror();
-		if (error) {
-			printf("%s\n", error);
-		}
-		fn_handle = dlsym(dl_handle, ATPY_PASS);
-		error = dlerror();
-		if (error) {
-			printf("Error loading %s from %s: %s\n", ATPY_PASS, lib_file, error);
-		}
-		printf("Loaded %p\n", fn_handle);
-		LibraryListPtr = (struct LibraryListElement *)malloc(sizeof(struct LibraryListElement));
-		LibraryListPtr->Next = LibraryList;
-		LibraryListPtr->MethodName = fn_name;
-		LibraryListPtr->FunctionHandle = fn_handle;
-		LibraryList = LibraryListPtr;
-	}
-	pass_function pfn;
-	pfn = (pass_function) fn_handle;
-	pfn(rin, num_particles, element, param);
-	return 0;
+    char *lib_file = malloc(sizeof(char) * 1000);
+    struct LibraryListElement *LibraryListPtr;
+    void *fn_handle;
+    strcpy(lib_file, INTEGRATOR_PATH);
+    strcat(lib_file, fn_name);
+    strcat(lib_file, ".so");
+    LibraryListPtr = SearchLibraryList(LibraryList, fn_name);
+    if (LibraryListPtr) {
+        fn_handle = LibraryListPtr->FunctionHandle;
+    } else {
+        printf("Trying to load %s\n", lib_file);
+        void *dl_handle = dlopen(lib_file, RTLD_LAZY);
+        char *error = dlerror();
+        if (error) {
+            printf("%s\n", error);
+        }
+        fn_handle = dlsym(dl_handle, ATPY_PASS);
+        error = dlerror();
+        if (error) {
+            printf("Error loading %s from %s: %s\n", ATPY_PASS, lib_file, error);
+        }
+        printf("Loaded %p\n", fn_handle);
+        LibraryListPtr = (struct LibraryListElement *)malloc(sizeof(struct LibraryListElement));
+        LibraryListPtr->Next = LibraryList;
+        LibraryListPtr->MethodName = fn_name;
+        LibraryListPtr->FunctionHandle = fn_handle;
+        LibraryList = LibraryListPtr;
+    }
+    pass_function pfn;
+    pfn = (pass_function) fn_handle;
+    pfn(rin, num_particles, element, param);
+    return 0;
 }
 
 
 int pass_element(double *rin, int num_particles, PyObject *element, struct parameters *param) {
-	if (!PyObject_HasAttrString(element, "pass_method")) {
-		printf("No pass method.\n");
-		return 1;
-	}
-	PyObject *fn_name_object = PyObject_GetAttrString(element, "pass_method");
-	char *fn_name = PyString_AsString(fn_name_object);
-	call_pass_method(rin, num_particles, element, fn_name, param);
-	return 0;
+    if (!PyObject_HasAttrString(element, "pass_method")) {
+        printf("No pass method.\n");
+        return 1;
+    }
+    PyObject *fn_name_object = PyObject_GetAttrString(element, "pass_method");
+    char *fn_name = PyString_AsString(fn_name_object);
+    call_pass_method(rin, num_particles, element, fn_name, param);
+    return 0;
 }
 
 
@@ -96,76 +96,76 @@ int pass_element(double *rin, int num_particles, PyObject *element, struct param
  *  - num_turns: int number of turns to simulate
  */
 static PyObject *at_atpass(PyObject *self, PyObject *args) {
-	PyObject *element_list;
-	PyArrayObject *rin;
-	double **arin;
-	int num_turns;
-	int i, j;
-	struct parameters param;
-	param.nturn = 0;
-	param.mode = 0;
-	param.T0 = 0;
-	param.RingLength = 0;
+    PyObject *element_list;
+    PyArrayObject *rin;
+    double **arin;
+    int num_turns;
+    int i, j;
+    struct parameters param;
+    param.nturn = 0;
+    param.mode = 0;
+    param.T0 = 0;
+    param.RingLength = 0;
 
-	if (!PyArg_ParseTuple(args, "O!O!i", &PyList_Type, &element_list, &PyArray_Type, &rin, &num_turns)) {
-		PyErr_SetString(PyExc_ValueError, "Failed to parse arguments to atpass");
-		return NULL;
-	}
-	if (!PyArray_Check(rin)) {
-		PyErr_SetString(PyExc_ValueError, "Not a numpy array.");
-		return NULL;
-	}
+    if (!PyArg_ParseTuple(args, "O!O!i", &PyList_Type, &element_list, &PyArray_Type, &rin, &num_turns)) {
+        PyErr_SetString(PyExc_ValueError, "Failed to parse arguments to atpass");
+        return NULL;
+    }
+    if (!PyArray_Check(rin)) {
+        PyErr_SetString(PyExc_ValueError, "Not a numpy array.");
+        return NULL;
+    }
 
-	npy_intp dims[2];
-	PyArray_Descr *descr;
-	descr = PyArray_DescrFromType(NPY_DOUBLE);
-	if (!PyArray_AsCArray((PyObject **)&rin, (void *)&arin, dims, 2, descr) < 0) {
-		PyErr_SetString(PyExc_ValueError, "Could not convert into numpy array");
-		return NULL;
-	}
-	if (dims[1] != 6) {
-		PyErr_SetString(PyExc_ValueError, "Numpy array is not 6D");
-		return NULL;
-	}
+    npy_intp dims[2];
+    PyArray_Descr *descr;
+    descr = PyArray_DescrFromType(NPY_DOUBLE);
+    if (!PyArray_AsCArray((PyObject **)&rin, (void *)&arin, dims, 2, descr) < 0) {
+        PyErr_SetString(PyExc_ValueError, "Could not convert into numpy array");
+        return NULL;
+    }
+    if (dims[1] != 6) {
+        PyErr_SetString(PyExc_ValueError, "Numpy array is not 6D");
+        return NULL;
+    }
 
-	int num_elements = PyList_Size(element_list);
-	printf("There are %d elements in the list\n", num_elements);
-	printf("There are %d particles\n", dims[0]);
-	printf("Going for %d turns\n", num_turns);
-	for (i = 0; i < num_turns; i++) {
-		param.nturn = i;
-		for (j = 0; j < num_elements; j++) {
-			PyObject *element = PyList_GetItem(element_list, j);
-			pass_element(*arin, dims[0], element, &param);
-		}
-	}
-	return Py_BuildValue("i", 1);
+    int num_elements = PyList_Size(element_list);
+    printf("There are %d elements in the list\n", num_elements);
+    printf("There are %d particles\n", dims[0]);
+    printf("Going for %d turns\n", num_turns);
+    for (i = 0; i < num_turns; i++) {
+        param.nturn = i;
+        for (j = 0; j < num_elements; j++) {
+            PyObject *element = PyList_GetItem(element_list, j);
+            pass_element(*arin, dims[0], element, &param);
+        }
+    }
+    return Py_BuildValue("i", 1);
 }
 
 /* Boilerplate to register methods. */
 
 static PyMethodDef AtMethods[] = {
-	{"atpass",  at_atpass, METH_VARARGS,
-	"Python clone of atpass"},
-	{NULL, NULL, 0, NULL}        /* Sentinel */
+    {"atpass",  at_atpass, METH_VARARGS,
+    "Python clone of atpass"},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
 PyMODINIT_FUNC
 initat(void) {
-	import_array();
-	(void) Py_InitModule("at", AtMethods);
+    import_array();
+    (void) Py_InitModule("at", AtMethods);
 }
 
 
 int main(int argc, char *argv[]) {
-	/* Pass argv[0] to the Python interpreter */
-	Py_SetProgramName(argv[0]);
+    /* Pass argv[0] to the Python interpreter */
+    Py_SetProgramName(argv[0]);
 
-	/* Initialize the Python interpreter.  Required. */
-	Py_Initialize();
+    /* Initialize the Python interpreter.  Required. */
+    Py_Initialize();
 
-	/* Add a static module */
-	initat();
-	return 0;
+    /* Add a static module */
+    initat();
+    return 0;
 }
 
