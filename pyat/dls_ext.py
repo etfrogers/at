a43efcd7a043be60ede5afa15f5cbec84f9c4a92
Will Rogers <wahrogers@gmail.com>
2016-09-14 14:24:43 +0100

    Update dls test with new element format.

--- pyat/dls_ext.py
+++ pyat/dls_ext.py
@@ -20,32 +20,33 @@ PASS_METHODS = {'SEXT': 'StrMPoleSymplectic4Pass',
 
 for element in the_ring:
     family = element.family
+    element.Length = element.length
     if family == 'BEND':
-        element.bending_angle = 0.1309
-        element.entrance_angle = 0.0654
-        element.exit_angle = 0.0654
+        element.bending_angle = 0.130899693899575
+        element.entrance_angle = 0.065449846949787
+        element.exit_angle = 0.065449846949787
         element.gap = 0.0466
-        element.fringe_int_1 = 0.6438
-        element.fringe_int_2 = 0.6438
+        element.fringe_int_1 = 0.643776824034335
+        element.fringe_int_2 = 0.643776824034335
         element.max_order = 3
         element.num_int_steps = 10
-        element.polynom_a = numpy.array([0,0,0,0])
-        element.polynom_b = numpy.array([0,0,0,0])
+        element.polynom_a = numpy.array([0,0,0,0], dtype=numpy.float64)
+        element.polynom_b = numpy.array([0,0,0,0], dtype=numpy.float64)
     elif family == 'SEXT':
-        element.max_order = 3
-        element.num_int_steps = 10
-        element.polynom_a = numpy.array([0,0,0,0])
-        element.polynom_b = numpy.array([0,0,element.k2,0])
+        element.MaxOrder = 3
+        element.NumIntSteps = 10
+        element.PolynomA = numpy.array([0,0,0,0], dtype=numpy.float64)
+        element.PolynomB = numpy.array([0,0,element.k2,0], dtype=numpy.float64)
+    elif family == 'QUAD':
+        element.K = element.k1
     try:
-        element.pass_method = PASS_METHODS[family]
+        element.PassMethod = PASS_METHODS[family]
     except KeyError:
-        element.pass_method = 'DriftPass'
+        element.PassMethod = 'DriftPass'
 
 rin = numpy.zeros((1, 6))
 rin[0,0] = 1e-6
-print(rin)
 t = time.time()
 at.atpass(the_ring, rin, 1000)
 print("Time taken: {}".format(time.time() - t))
-
 print(rin)
