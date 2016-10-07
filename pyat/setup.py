58bd642264c64f4c7374e48220a30caa1ce868a0
Will Rogers <wahrogers@gmail.com>
2016-10-07 11:17:20 +0000

    Use abspath for integrator paths.

--- pyat/setup.py
+++ pyat/setup.py
@@ -10,12 +10,12 @@ mach_home = os.getenv('MACH_HOME')
 mach_arch = os.getenv('MACHARCH')
 pyversion = "python{0.major}.{0.minor}".format(sys.version_info)
 
-integrator_src = '../atintegrators'
+integrator_src = os.path.abspath('../atintegrators')
 integrator_build = None
 
 for pass_method in glob.glob(os.path.join(integrator_src, '*Pass.c')):
     print(pass_method)
-    x = setup(name=pass_method[:-2], ext_modules=[Extension(name=pass_method[:-2], sources=[pass_method], define_macros=[('PYAT', None)], include_dirs=[numpy.get_include(), '../atintegrators'])])
+    x = setup(name=pass_method[:-2], ext_modules=[Extension(name=pass_method[:-2], sources=[pass_method], define_macros=[('PYAT', None)], include_dirs=[numpy.get_include(), integrator_src])])
     print(x.command_obj['install'].install_platlib)
     if integrator_build is not None:
         integrator_build = x.command_obj['install'].install_platlib
