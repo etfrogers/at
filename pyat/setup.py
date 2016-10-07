50d8bdd0256db1ff20d3d9a3a1d9821b0f6ae1e0
Will Rogers <wahrogers@gmail.com>
2016-10-07 11:57:29 +0100

    Locate integrator build path in setup.py

--- pyat/setup.py
+++ pyat/setup.py
@@ -9,18 +9,21 @@ macros=[]
 mach_home = os.getenv('MACH_HOME')
 mach_arch = os.getenv('MACHARCH')
 pyversion = "python{0.major}.{0.minor}".format(sys.version_info)
-if (mach_home is not None and mach_arch is not None):
-    integrator_path = '"' + os.path.join(mach_home, 'lib', mach_arch, pyversion, 'atintegrators') + '"'
-    macros.append(('INTEGRATOR_PATH', integrator_path))
-else:
-    integrator_path = '../atintegrators'
+
+integrator_src = '../atintegrators'
+integrator_build = None
+
+for pass_method in glob.glob(os.path.join(integrator_src, '*Pass.c')):
+    print(pass_method)
+    x = setup(name=pass_method[:-2], ext_modules=[Extension(name=pass_method[:-2], sources=[pass_method], define_macros=[('PYAT', None)], include_dirs=[numpy.get_include(), '../atintegrators'])])
+    print(x.command_obj['install'].install_platlib)
+    if integrator_build is not None:
+        integrator_build = x.command_obj['install'].install_platlib
+        macros.append(('INTEGRATOR_PATH', integrator_build))
 
 at = Extension('at', sources=['at.c'],
                define_macros=macros,
-               include_dirs=[numpy.get_include(), '../atintegrators'])
+               include_dirs=[numpy.get_include(), integrator_src])
 
 setup(name='at', py_modules=['elements','load_mat'], ext_modules=[at])
 
-for pass_method in glob.glob(os.path.join(integrator_path, '*Pass.c')):
-    print(pass_method)
-    setup(name=pass_method[:-2], ext_modules=[Extension(name=pass_method[:-2], sources=[pass_method], define_macros=[('PYAT', None)], include_dirs=[numpy.get_include(), '../atintegrators'])])
